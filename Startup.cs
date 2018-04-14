c40e280a0d4cd3748e824eace0aa220adbe3d21b
azeezrasaq <azeezrasaq@icloud.com>
2018-04-14 11:58:44 +0100

    Push web project to remote server

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(softwareproject.Startup))]
+namespace softwareproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
